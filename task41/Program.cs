ece9b749bc69893e739ae465133792f89aa626a0
Manecoon <stas_on.85@mail.ru>
2023-01-14 22:18:23 +0300

    tasks

---
+++ task41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] Array()
+{
+    Console.Write($"Введите длину массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[m];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+        Console.Write($"{array[i]} ");
+    }
+    return array;
+}
+int[] array = Array();
+
+int Kol(int[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] > 0) count++;
+    }
+    return count;
+}
+
+void Print()
+{
+    Console.WriteLine();
+    Console.WriteLine($"Количество чисел больше нуля: {Kol(array)}");
+}
+
+Kol(array);
+Print();
---
+++ task43/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Введите значение b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[2, 3];
+array[0, 2] = b1;
+array[0, 0] = -k1;
+array[1, 2] = b2;
+array[1, 0] = -k2;
+array[0, 1] = 1;
+array[1, 1] = 1;
+
+void Ptint()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"[{array[i, j]}] ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void GausFaza()
+{
+    double t = (array[1, 0] / array[0, 0]) * (-1);
+
+    array[1, 0] = array[0, 0] * t + array[1, 0];
+    array[1, 1] = array[0, 1] * t + array[1, 1];
+    array[1, 2] = array[0, 2] * t + array[1, 2];
+}
+
+void GausRes()
+{
+    double y = array[1, 2] / array[1, 1];
+
+    double x = (array[0, 2] - y) / array[0, 0];
+    Console.Write($"Точка пересечения двух прямых: [{y}], [{x}]");
+}
+Console.WriteLine($" Матрица Гауса фаза1:");
+Ptint();
+GausFaza();
+Console.WriteLine($" Матрица Гауса фаза2:");
+Ptint();
+GausRes();
+// Округлять десятичные дроби не стал сознательно, так как ответы дробей с периодами, будут округляться. Лучше пусть видят дроби полные
